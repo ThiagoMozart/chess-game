71d2a7f73ebe65021ef24c1c156e23f6afea2b27
Matheus Aragão <matheusaragao@id.uff.br>
2022-04-10 12:27:17 -0300

    Iniciando o projeto

---
+++ src/components/Menu.jsx
@@ -0,0 +1,18 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+
+function Menu() {
+  return (
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">XadrezAI - Grupo 7</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#game">Sobre o jogo</Nav.Link>
+            <Nav.Link href="#contact">Contato</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  );
+}
+
+export default Menu;
