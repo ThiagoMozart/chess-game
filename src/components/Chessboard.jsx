1cc8cc8f12fbd6796a2ca5cd41c80fafb84d2d92
Matheus Aragão <matheusaragao@id.uff.br>
2022-04-18 00:47:37 -0300

    Modificações + branch Pecas

--- src/components/Chessboard.jsx
+++ src/components/Chessboard.jsx
@@ -1,53 +1,69 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { fillBoardWithPieces, mountBoard } from '../utils/utils';
 
 import './styles/Chessboard.css'
 
-// Eixos do tabuleiro
-//const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"]
-//const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"]
-
 export default function Chessboard() {
-    const [board, setBoard] = useState([]);
-    const [positions] = useState(
-    [{fromPlayer: true, position: 'x1y0', type:'peao'}, {fromPlayer: true, position: 'x1y1', type:'peao'}, {fromPlayer: true, position: 'x1y2', type:'peao'}, {fromPlayer: true, position: 'x1y3', type:'peao'}, {fromPlayer: true, position: 'x1y4', type:'peao'}, {fromPlayer: true, position: 'x1y5', type:'peao'}, {fromPlayer: true, position: 'x1y6', type:'peao'}, {fromPlayer: true, position: 'x1y7', type:'peao'},
-    {fromPlayer: true, position: 'x0y0', type:'torre'}, {fromPlayer: true, position: 'x0y1', type:'cavalo'}, {fromPlayer: true, position: 'x0y2', type:'bispo'}, {fromPlayer: true, position: 'x0y3', type:'rainha'}, {fromPlayer: true, position: 'x0y4', type:'rei'}, {fromPlayer: true, position: 'x0y5', type:'bispo'}, {fromPlayer: true, position: 'x0y6', type:'cavalo'}, {fromPlayer: true, position: 'x0y7', type:'torre'},
-    {fromPlayer: false, position: 'x6y0', type:'peao'}, {fromPlayer: false, position: 'x6y1', type:'peao'}, {fromPlayer: false, position: 'x6y2', type:'peao'}, {fromPlayer: false, position: 'x6y3', type:'peao'}, {fromPlayer: false, position: 'x6y4', type:'peao'}, {fromPlayer: false, position: 'x6y5', type:'peao'}, {fromPlayer: false, position: 'x6y6', type:'peao'}, {fromPlayer: false, position: 'x6y7', type:'peao'},]);
+    const [board, setBoard] = useState(mountBoard());
+    const [pieces] = useState(fillBoardWithPieces())   
     const [firstRender, setFirstRender] = useState(false);
     
     useEffect(() => {
         setFirstRender(true);
     }, []);
 
-    const loadBoard = useCallback(() => {
-        // Renderiza eixos do tabuleiro (Da esquerda para a direita | a - h |) & (De cima para baixo | 8 - 1 |)
-        for (let j = 7; j >= 0; j--){
-            for (let i = 0; i < 8; i++){
-                const number = j + i + 2;
-
-                if (number % 2 === 0){
-                    board.push({position: `x${j}y${i}`, squareColor: 'black-square', element: <div key={`x${j}y${i}`} className="black-square"></div>})
-                }
-                else{
-                    board.push({position: `x${j}y${i}`, squareColor: 'white-square',element: <div key={`x${j}y${i}`} className="white-square"></div>})
-                }
-            }
-        }
-    }, [board]);
-
     useEffect(() => {
         if (!firstRender) {
-            loadBoard();
+            mountBoard();
         }
-    }, [loadBoard, firstRender]);
+    }, [firstRender]);
 
     useEffect(() => {
+        setBoard(mountBoard())
         let newBoard = [...board];
-        positions.forEach(y => {
+        pieces.forEach(y => {
             const foundItem = board.findIndex(x => x.position === y.position)
-            newBoard[foundItem].element = <div style={{ color: 'red' }} key={board[foundItem].position} className={board[foundItem].squareColor}>{y.type}</div>
+            if(newBoard[foundItem]) {
+                newBoard[foundItem].element =
+                <div style={{ color: 'red', textAlign: 'center' }} key={board[foundItem].position} id={board[foundItem].position} className={board[foundItem].squareColor}>
+                    <div className="text">
+                        {y.type}
+                    </div>
+                    <img alt="" src={y.imagePath} loading="lazy"/>
+                </div>
+            }
         })
         setBoard(newBoard);
-    }, [positions]) // eslint-disable-line react-hooks/exhaustive-deps
+    }, [pieces]) // eslint-disable-line react-hooks/exhaustive-deps
+
+    
+    //Drag and drop em ambiente reativo não é tão simples, precisamos conversar sobre antes de implementar uma abordagem de drag and drop e usar um componente próprio para isso
+    //como o react-dnd por exemplo, mas recomendo usarmos uma abordagem de destinos pre selecionados com auto animação pela simplicidade.
+
+    // function grabPiece(e){
+    //     const element = e.target.parentElement;
+        
+    //     if (element.outerHTML.includes("img")){
+    //         setActivePiece(element);
+    //     }
+    // }
+
+    // function movePiece (e){
+    //     if (activePiece){
+    //         var x = e.clientX, y = e.clientY,
+    //          elementMouseIsOver = document.elementFromPoint(x, y);
+    //          if(elementMouseIsOver.outerHTML.includes("div")) {                
+    //             let newPieces = [...pieces];
+    //             const foundItem = pieces.findIndex(x => x.position == activePiece.id);
+    //             const oldItem = pieces.findIndex(x => x.position == elementMouseIsOver.id);
+    //             newPieces[foundItem].position = elementMouseIsOver.id
+    //             setPieces(newPieces);
+    //          }
+    //     }
+    // }
+    // function dropPiece(e){
+    //     setActivePiece(null);
+    // }
 
     return <div id="chessboard">
         {board.map((x, index) => {
---
+++ src/schemas/Piece.js
@@ -0,0 +1,9 @@
+
+export default class Piece {
+    constructor (fromPlayer, position, type, imagePath) {
+        this.fromPlayer = fromPlayer;
+        this.position = position;
+        this.type = type;
+        this.imagePath = imagePath;
+    }
+}
