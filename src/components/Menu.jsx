d1533acb5287a7a26caa0b481d0ce39e730d5191
Matheus Aragão <matheusaragao@id.uff.br>
2022-05-07 13:38:38 -0300

    Mudança número do grupo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ function Menu() {
   return (
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">XadrezAI - Grupo 7</Navbar.Brand>
+          <Navbar.Brand href="#home">XadrezAI - Grupo 6</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#game">Sobre o jogo</Nav.Link>
