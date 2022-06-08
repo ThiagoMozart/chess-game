833853e01e2ead8c5e2bd415b26bdb23fb89fd18
Camila-Ferr <camilaferralves@gmail.com>
2022-06-07 22:01:08 -0300

    Finalização

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -26,8 +26,10 @@ export default function GameOver(props) {
         </h1>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={() => refreshGame()}>Reiniciar o jogo</Button>
+        <Button variante ="primary" onClick={() => refreshGame()}>Reiniciar o jogo</Button>
       </Modal.Footer>
     </Modal>
   );
 }
+
+
