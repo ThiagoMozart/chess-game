e025f69982ec265456f1cd81b43e6fcc53607cf0
mozart <thiagogarciamozart@gmail.com>
2022-07-03 18:20:29 -0300

    Feature: Github integration and more unit tests

--- src/components/test/utils.test.js
+++ src/components/test/utils.test.js
@@ -1,10 +1,10 @@
 import "@testing-library/jest-dom"
 import Piece from "../../schemas/Piece.js"
-import { fillBoardWithPieces } from "../../utils/utils.js";
+import { fillBoardWithPieces, mountBoard, getPeonToEvolve, checkIfPositionIsEmpty } from "../../utils/utils.js";
 
 test("Should have same number of objects in array as pieces in an usual chess game", () => {
-    const usualNumberOfPiecesInChess = Array(32).fill(new Piece());
-    expect(fillBoardWithPieces().length).toBe(usualNumberOfPiecesInChess.length);
+    const usualNumberOfPiecesInChess = 32;
+    expect(fillBoardWithPieces().length).toBe(usualNumberOfPiecesInChess);
   });
 
 test("Should have the player king piece and the IA king piece", () => {
@@ -14,4 +14,24 @@ test("Should have the player king piece and the IA king piece", () => {
         expect.objectContaining(new Piece(28, false, 'x7y4', 'rei'))
       ])
     );
+});
+
+test("Should return if a peon can evolve", () => {
+  const board = mountBoard();
+  const pieces = fillBoardWithPieces();
+  
+  expect(getPeonToEvolve(pieces, board)).toBe(null);
+
+  pieces[0] = new Piece(0, true, 'x7y3', 'peao');
+  pieces[27] = new Piece(27, false, 'x1y0', 'rainha');
+
+  expect(getPeonToEvolve(pieces, board)).toBe(pieces[0]);
+
+});
+
+test("Check if position is empty", () => {
+  const pieces = fillBoardWithPieces();
+
+  expect(checkIfPositionIsEmpty('x7y3', pieces)).toBe(false);
+  expect(checkIfPositionIsEmpty('x5y3', pieces)).toBe(true);
 });
