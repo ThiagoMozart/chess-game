eec317dcbc2308d73e3abc2648f3ed6a7ceed897
Matheus Aragão <matheusaragao@id.uff.br>
2022-07-06 18:42:07 -0300

    fix

--- src/components/test/Menu.test.jsx
+++ src/components/test/Menu.test.jsx
@@ -1,21 +1,14 @@
-import { cleanup, queryByText, render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom"
-import Menu from '../Menu.jsx'
-import React from 'react'
+import { cleanup, queryByText, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Menu from "../Menu.jsx";
+import React from "react";
 
+describe("Menu Component", () => {
+  it("Should render menu", () => {
+    const { getByText } = render(<Menu />);
 
-
-
-describe('Menu Component', () => {
-
-    it('Should render menu', () => {
-
-        const {getByText} = render(<Menu />);
-
-        expect(getByText('XadrezAI - Grupo 6')).toBeInTheDocument();
-        expect(getByText('Home')).toBeInTheDocument();
-        expect(getByText('Sobre o jogo')).toBeInTheDocument();
-
-    });
-
+    expect(getByText("XadrezAI - Grupo 6")).toBeInTheDocument();
+    expect(getByText("Home")).toBeInTheDocument();
+    expect(getByText("Sobre o jogo")).toBeInTheDocument();
+  });
 });
